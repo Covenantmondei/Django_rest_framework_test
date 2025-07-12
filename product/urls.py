11e04a9a096ada2285c0b7c9ff5d4db25cdb1438
Covenant Monday <covenantmonday863@gmail.com>
2025-07-12 21:16:43 +0100

    updated endpoints

--- product/urls.py
+++ product/urls.py
@@ -4,4 +4,6 @@ from .views import *
 urlpatterns = [
     path('hello/', Hello.as_view(), name="hello"),
     path('form/', Logistic.as_view(), name="logistic"),
+    path('create-school', create_school, name="Create School"),
+    path('create-student', create_student, name="Create Student")
 ]
