b79fcf3ef6cd135774bbaa7205437c21fc1416f6
Covenant Monday <covenantmonday863@gmail.com>
2025-07-12 21:16:19 +0100

    updated serializers

--- product/serializers.py
+++ product/serializers.py
@@ -1,17 +1,25 @@
 from rest_framework import serializers
-from .models import Product, Student
+from .models import Product, Student, School
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__' 
 
-class SchoolSerializer(serializers.Serializer):
-    name = serializers.CharField()
-    branch = serializers.CharField()
+class SchoolSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = School
+        fields = '__all__'
 
-    def create():
-        pass
+    def create(self, validated_data):
+        return School.objects.create(**validated_data)
+
+
 
 class StudentSerializer(serializers.Serializer):
-    name = serializers.CharField()
+    name = serializers.CharField
+
+    def create(self, validated_data):
+        school = self.context['school']
+        school = School.objects.get(id=school)
+        return Student.objects.create(school_id=school , **validated_data)
