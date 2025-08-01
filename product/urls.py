3cd57937c21da946d906c76e12760a51e95ada52
Covenant Monday <covenantmonday863@gmail.com>
2025-08-01 17:37:04 +0100

    modified the urls

--- product/urls.py
+++ product/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('hello/', Hello.as_view(), name="hello"),
     path('form/', Logistic.as_view(), name="logistic"),
     path('create-school', create_school, name="Create School"),
-    path('create-student', create_student, name="Create Student")
+    path('create-student', create_student, name="Create Student"),
+    path('hello-world', HelloWorld.as_view(), name="Helloo World"),
+    path('market', MarketList.as_view(), name="marketlist"),
+    path('create-market', CreateMarket.as_view(), name="create")
 ]
